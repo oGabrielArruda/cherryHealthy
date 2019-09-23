8724ef7e07532e624eaeb8c3a3f788bafc175ba5
Felipe Pires <48968398+piresflp@users.noreply.github.com>
2019-09-23 08:25:24 -0300

    Mudanças css

--- js/home.js
+++ js/home.js
@@ -9,3 +9,31 @@ $(document).ready(function () {
 
 });
 
+$(document).ready(function() {
+  var NavTop = $('.nav').offset().top;
+  var Nav = function() {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > NavTop) { 
+      $('.nav').addClass('sticky');
+    } else {
+      $('.nav').removeClass('sticky'); 
+    }
+  };
+
+Nav();
+
+  $(window).scroll(function() {
+    Nav();
+  });
+});
+
+$(window).scroll(function() {    
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 100) {
+      $("#Top_bar").addClass("is-sticky");
+  } else {
+      $("#Top_bar").removeClass("is-sticky");
+  }
+});
+
