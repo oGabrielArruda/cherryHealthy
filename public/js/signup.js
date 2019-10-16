8d461cf6b8e8ee14cdffb4de827b02b215fd23cf
Felipe Pires <48968398+piresflp@users.noreply.github.com>
2019-10-16 09:21:56 -0300

    cadastro corrigido

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,4 +2,37 @@ $(document).ready(function () {
     $('select').formSelect();
 });
 
+function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+}
+function fMascEx() {
+    obj.value=masc(obj.value)
+}
 
+function mTel(tel) {
+    tel=tel.replace(/\D/g,"")
+    tel=tel.replace(/^(\d)/,"($1")
+    tel=tel.replace(/(.{3})(\d)/,"$1)$2")
+    if(tel.length == 9) {
+        tel=tel.replace(/(.{1})$/,"-$1")
+    } else if (tel.length == 10) {
+        tel=tel.replace(/(.{2})$/,"-$1")
+    } else if (tel.length == 11) {
+        tel=tel.replace(/(.{3})$/,"-$1")
+    } else if (tel.length == 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    } else if (tel.length > 12) {
+        tel=tel.replace(/(.{4})$/,"-$1")
+    }
+    return tel;
+}
+
+function mCPF(cpf){
+    cpf=cpf.replace(/\D/g,"")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    return cpf
+}
