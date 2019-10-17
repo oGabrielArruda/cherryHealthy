64af964e8d8210c8eace93018ad3563c6c9da4fd
Gabriel Arruda <gabrielalvesdearruda@gmail.com>
2019-10-17 08:30:37 -0300

    + dieta

--- public/js/dieta.js
+++ public/js/dieta.js
@@ -7,17 +7,27 @@ $(document).ready(function(){
             $(".dia:eq("+i+")").removeClass("active");
         }
         $(this).addClass("active");
-
-        var url = 'http://localhost:3000/dieta';
+        var url = 'http://localhost:3000/dieta/';
         var dia = $(this).text().toLowerCase();
-        console.log(dia);
+        var arr;
         $.getJSON(url, function(result){
-            var arr = result;
-            $("#textarea1").val(arr[0].dia);
+            arr = result;        
+            switch(dia){
+                case 'seg':
+                   $("#textarea1").val(arr[0].seg);
+                case 'ter':
+                   $("#textarea1").val(arr[0].ter);
+                case 'qua':
+                   $("#textarea1").val(arr[0].qua);
+                case 'qui':
+                   $("#textarea1").val(arr[0].qui);
+                case 'sex':
+                   $("#textarea1").val(arr[0].sex);
+                case 'sab':
+                   $("#textarea1").val(arr[0].sab);
+                case 'dom':
+                   $("#textarea1").val(arr[0].dom);
+            }
         });
-        
-        switch(dia){
-            case 'seg':
-        }
     });
 });
