bdd56ab75cafe4dc369a9044eda5da8f24923b6f
Gabriel Arruda <gabrielalvesdearruda@gmail.com>
2019-10-17 17:51:28 -0300

    dieta funfs

--- public/js/dieta.js
+++ public/js/dieta.js
@@ -11,20 +11,26 @@ $(document).ready(function(){
         var dia = $(this).text().toLowerCase();
         var arr;
         $.getJSON(url, function(result){
-            arr = result;        
+            arr = result;      
             switch(dia){
                 case 'seg':
                    $("#textarea1").val(arr[0].seg);
+                   break;
                 case 'ter':
                    $("#textarea1").val(arr[0].ter);
+                   break;
                 case 'qua':
                    $("#textarea1").val(arr[0].qua);
+                   break;
                 case 'qui':
                    $("#textarea1").val(arr[0].qui);
+                   break;
                 case 'sex':
                    $("#textarea1").val(arr[0].sex);
+                   break;
                 case 'sab':
                    $("#textarea1").val(arr[0].sab);
+                   break;
                 case 'dom':
                    $("#textarea1").val(arr[0].dom);
             }
