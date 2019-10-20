4f4c1d01b988bb66361a04fb509450afde0b0445
Gabriel Arruda <gabrielalvesdearruda@gmail.com>
2019-10-20 16:04:55 -0300

    alteracoes perfil

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
     start();
 });
 
-function start(){
+function start() {
     var url = 'http://localhost:3000/usuarioPerfil/';
-    $.getJSON(url, function(result){
+    $.getJSON(url, function (result) {
         var arr = result;
-
+        $("#welcome").text("Bem-vindo " + arr[0].nome);
         $("#name").val(arr[0].nome);
         $("#email").val(arr[0].email);
         $("#telefone").val(arr[0].telefone);
@@ -16,37 +16,37 @@ function start(){
     });
 }
 
-function fMasc(objeto,mascara) {
-    obj=objeto
-    masc=mascara
-    setTimeout("fMascEx()",1)
+function fMasc(objeto, mascara) {
+    obj = objeto
+    masc = mascara
+    setTimeout("fMascEx()", 1)
 }
 function fMascEx() {
-    obj.value=masc(obj.value)
+    obj.value = masc(obj.value)
 }
 
 function mTel(tel) {
-    tel=tel.replace(/\D/g,"")
-    tel=tel.replace(/^(\d)/,"($1")
-    tel=tel.replace(/(.{3})(\d)/,"$1)$2")
-    if(tel.length == 9) {
-        tel=tel.replace(/(.{1})$/,"-$1")
+    tel = tel.replace(/\D/g, "")
+    tel = tel.replace(/^(\d)/, "($1")
+    tel = tel.replace(/(.{3})(\d)/, "$1)$2")
+    if (tel.length == 9) {
+        tel = tel.replace(/(.{1})$/, "-$1")
     } else if (tel.length == 10) {
-        tel=tel.replace(/(.{2})$/,"-$1")
+        tel = tel.replace(/(.{2})$/, "-$1")
     } else if (tel.length == 11) {
-        tel=tel.replace(/(.{3})$/,"-$1")
+        tel = tel.replace(/(.{3})$/, "-$1")
     } else if (tel.length == 12) {
-        tel=tel.replace(/(.{4})$/,"-$1")
+        tel = tel.replace(/(.{4})$/, "-$1")
     } else if (tel.length > 12) {
-        tel=tel.replace(/(.{4})$/,"-$1")
+        tel = tel.replace(/(.{4})$/, "-$1")
     }
     return tel;
 }
 
-function mCPF(cpf){
-    cpf=cpf.replace(/\D/g,"")
-    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
-    cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+function mCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
     return cpf
 }
