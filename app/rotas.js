18b44a42c168df16d562d38a7a0b79f1d5eda13a
Gabriel Arruda <gabrielalvesdearruda@gmail.com>
2019-10-31 09:57:47 -0300

    login funfs

--- app/rotas.js
+++ app/rotas.js
@@ -8,6 +8,22 @@ module.exports = (app) => {
         localStorage = new LocalStorage('./scratch');
     }
     var path = require('path');
+    
+    const crypto = require('crypto');
+    
+    const DADOS_CRIPTOGRAFAR = {
+        algoritmo: "aes256",
+        codificacao: "utf8",
+        segredo: "chaves",
+        tipo: "hex"
+    };
+    
+    function criptografar(senha) {
+        const cipher = crypto.createCipher(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo);
+        cipher.update(senha);
+        return cipher.final(DADOS_CRIPTOGRAFAR.tipo);
+    }
+    
 
     function execSQL(sql, resposta) {
         global.conexao.request()
